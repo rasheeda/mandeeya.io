5bfad36e252d7dfe5c0b30765a8cd17f70bd1f28
Rasheeda Mandeeya <ryehuza@gmail.com>
2020-04-23 00:09:37 +0200

    adding build folder

---
+++ build/precache-manifest.a6cfcb3acdc57325a6195de9cd2aa6ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3cd6bf0391a3ee9db4a68b46dbbec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e8bd107e1dc8097ee23",
+    "url": "/static/css/main.cf56fe04.chunk.css"
+  },
+  {
+    "revision": "5e7d77bca291cc419ade",
+    "url": "/static/js/2.6cec75e3.chunk.js"
+  },
+  {
+    "revision": "9e8bd107e1dc8097ee23",
+    "url": "/static/js/main.f5c3e29f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
